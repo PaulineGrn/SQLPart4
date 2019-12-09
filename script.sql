dbf1aab1ef1d596e7ea8fc04d2f2449e4e56f4f6
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-09 14:34:36 +0100

    SQLPart4

--- script.sql
+++ script.sql
@@ -2,11 +2,21 @@
 --Ex1
 USE `webdevelopement` ;
 --Insérer dans la table languages avec 2 colonnes (versions, version) avec différentes VALUES
-INSERT INTO `languages`(versions, version) VALUES ('JavaScript','5'), ('PHP', '5.2'), ('PHP', '5.4'), ('HTML', '5.1'), ('JavaScript', '6'), ('JavaScript', '7'), ('JavaScript', '8'), ('PHP', '7')
+INSERT INTO `languages`(`languages`, `version`)
+VALUES
+('JavaScript','5'),
+('PHP', '5.2'),
+('PHP', '5.4'),
+('HTML', '5.1'),
+('JavaScript', '6'),
+('JavaScript', '7'),
+('JavaScript', '8'),
+('PHP', '7');
 --Ex2
 USE `webdevelopement` ;
-INSERT INTO `frameworks`(name, version)
-VALUES ('Symfony','2.8'),
+INSERT INTO `frameworks`(`name`, `version`)
+VALUES
+('Symfony','2.8'),
 ('Symfony', '3'),
 ('Jquery', '1.6'),
 ('Jquery', '2.10');
